3599621c36d64b79446756c3cfbdd8f8f34adf3d
eylonx <eylon.perets@gmail.com>
2022-06-02 14:44:54 +0300

    login text added, missing conditionals

--- client/src/Components/Layout/Login/Login.tsx
+++ client/src/Components/Layout/Login/Login.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import './Login.css';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
-  return <div className="Login">Login</div>;
+  return (
+    <div className="Login">
+      {/* if logged out, show login and register */}
+      {/* otherwise, show hi x and logout button */}
+      <span>Hi user |</span>
+      <Link to="/logout">
+        <Button variant="text">Logout</Button>
+      </Link>
+      {/* 
+        <Link to="/login">Login</Login>
+        <Link to="/register">Register</Link>
+        */}
+    </div>
+  );
 }
