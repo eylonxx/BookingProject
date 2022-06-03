4962ba9d0b9692b9dbaeae8a341ae0b06fb12f71
Eylon <eylon.perets@gmail.com>
2022-06-03 21:12:47 +0300

    commented out seeds, uploaded db

--- client/src/Components/Vacations/VacationsList/VacationsList.tsx
+++ client/src/Components/Vacations/VacationsList/VacationsList.tsx
@@ -7,46 +7,46 @@ import './VacationsList.css';
 export default function VacationsList(): JSX.Element {
   const [vacations, setVacations] = useState<VacationModel[]>([]);
   //get vacations from vacationservice that uses axios to fetch from the server
-  // useEffect(
-  //   //save to local state or redux
-  //   () => {
-  //     vacationService
-  //       .getAllVacations()
-  //       .then((allVacations) => {
-  //         setVacations(allVacations);
-  //       })
-  //       .catch((e) => {
-  //         console.log(e);
-  //       });
+  useEffect(
+    //save to local state or redux
+    () => {
+      vacationService
+        .getAllVacations()
+        .then((allVacations) => {
+          setVacations(allVacations);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    []
+  );
+  // const vacationsSeeds = [
+  //   {
+  //     id: 1,
+  //     description: 'a place',
+  //     destination: 'israel',
+  //     imageName: '',
+  //     startingDate: '0000-00-00',
+  //     endingDate: '0000-00-00',
+  //     price: 14,
+  //     followers: 0,
   //   },
-  //   []
-  // );
-  const vacationsSeeds = [
-    {
-      id: 1,
-      description: 'a place',
-      destination: 'israel',
-      imageName: '',
-      startingDate: '0000-00-00',
-      endingDate: '0000-00-00',
-      price: 14,
-      followers: 0,
-    },
-    {
-      id: 2,
-      description: 'a point',
-      destination: 'japan',
-      imageName: 'x',
-      startingDate: '0000-00-00',
-      endingDate: '0000-00-00',
-      price: 53,
-      followers: 0,
-    },
-  ];
+  //   {
+  //     id: 2,
+  //     description: 'a point',
+  //     destination: 'japan',
+  //     imageName: 'x',
+  //     startingDate: '0000-00-00',
+  //     endingDate: '0000-00-00',
+  //     price: 53,
+  //     followers: 0,
+  //   },
+  // ];
 
   return (
     <div className="VacationsList">
-      {vacationsSeeds.map((vacation) => (
+      {vacations.map((vacation) => (
         <Vacation vacation={vacation} />
       ))}
     </div>
