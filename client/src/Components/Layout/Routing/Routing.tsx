2e08bc1bf92d9088a98d22c6e05dedcb6d4c300f
Eylon <eylon.perets@gmail.com>
2022-06-04 13:57:52 +0300

    created routes for register and login

--- client/src/Components/Layout/Routing/Routing.tsx
+++ client/src/Components/Layout/Routing/Routing.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import VacationsList from '../../Vacations/VacationsList/VacationsList';
+import LoginPage from '../../Auth/LoginPage/LoginPage';
+import RegisterPage from '../../Auth/RegisterPage/RegisterPage';
 
 export default function Routing() {
   return (
@@ -8,6 +10,8 @@ export default function Routing() {
       <Routes>
         <Route path="/" element={<h1>trrrrrrrrrrrrrrrrrrest</h1>} />
         <Route path="/vacations" element={<VacationsList />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
