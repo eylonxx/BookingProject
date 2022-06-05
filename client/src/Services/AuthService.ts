5d1b508f0561675ec93c634e566d3fd9fa2f847a
Eylon <eylon.perets@gmail.com>
2022-06-05 21:37:18 +0300

    added comments on authservice regarding loggingout

--- client/src/Services/AuthService.ts
+++ client/src/Services/AuthService.ts
@@ -7,14 +7,19 @@ class AuthService {
   public async register(user: UserModel): Promise<void> {
     const response = await axios.post<string>('http://localhost:3001/register', user);
     const token = response.data;
+    //save token to redux, login right after registration
   }
   //login
   public async login(credentials: CredentialsModel): Promise<void> {
     const response = await axios.post<string>('http://localhost:3001/login', credentials);
     const token = response.data;
+    //save token to redux
   }
   //logout
+  //get token, find user, remove token
+
   //isloggedin
+  //get token, verify token, used for middleware
 }
 const authService = new AuthService();
 export default authService;
