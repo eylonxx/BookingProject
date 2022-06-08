708979847492d998cb68836a38a2ede3b5e24df8
Eylon <eylon.perets@gmail.com>
2022-06-08 08:33:27 +0300

    new vacation form fe validation added

--- client/src/Components/Vacations/NewVacationForm/NewVacationForm.tsx
+++ client/src/Components/Vacations/NewVacationForm/NewVacationForm.tsx
@@ -16,17 +16,41 @@ export default function NewVacationForm() {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <label>Description:</label>
-      <input {...register('description')} />
+      <input
+        {...register('description', {
+          required: { value: true, message: 'Missing description' },
+        })}
+      />
       <label>Destination:</label>
-      <input {...register('destination')} />
+      <input
+        {...register('destination', {
+          required: { value: true, message: 'Missing destination' },
+        })}
+      />
       <label>imageName:</label>
       <input {...register('imageName')} />
       <label>Starting Date:</label>
-      <input type="date" {...register('startingDate')} />
+      <input
+        type="date"
+        {...register('startingDate', {
+          required: { value: true, message: 'Missing starting date' },
+        })}
+      />
       <label>endingDate:</label>
-      <input type="date" {...register('endingDate')} />
+      <input
+        type="date"
+        {...register('endingDate', {
+          required: { value: true, message: 'Missing ending date' },
+        })}
+      />
       <label>Price:</label>
-      <input {...register('price')} />
+      <input
+        {...register('price', {
+          required: { value: true, message: 'Missing price' },
+          min: { value: 0, message: "Price can't be negative" },
+          max: { value: 100000, message: "Price can't exceed 100,000" },
+        })}
+      />
       <button>Submit</button>
     </form>
   );
