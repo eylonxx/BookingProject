98d77e2a4c6db5a89b2c1522263730001e33b780
Eylon <eylon.perets@gmail.com>
2022-06-13 21:29:46 +0300

    idk

--- client/src/Components/Vacations/VacationsList/VacationsList.tsx
+++ client/src/Components/Vacations/VacationsList/VacationsList.tsx
@@ -8,9 +8,10 @@ import './VacationsList.css';
 export default function VacationsList(): JSX.Element {
   const [vacations, setVacations] = useState<VacationModel[]>([]);
   //how to get followed vacations?
-  //get user id (redux), use a service to obtain all vacations that a user follows from sql query.
-  //now i have all the vacations that a user follows
-  //pass
+  //setup redux for follows
+  //follow and unfollow functions
+  //on each vacation component have the follow and unfollow events call redux to change the state and change the heart to active\not active with redux.
+  //on redux have it pull all current followed vacations with a special sql query from the followers table
   const userToken = store.getState().authState.token;
 
   useEffect(
