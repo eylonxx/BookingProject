50b05304bdbe777d2dd36129087a586466ab080f
Eylon <eylon.perets@gmail.com>
2022-06-15 23:56:24 +0300

    removed more logs

--- server/src/6-controllers/vacations-controller.ts
+++ server/src/6-controllers/vacations-controller.ts
@@ -31,10 +31,7 @@ router.get('/vacations/:id', verifyLoggedIn, async (req: Request, res: Response,
 router.post('/vacations', async (req: Request, res: Response, next: NextFunction) => {
   try {
     //vacation info from form
-    console.log(req);
-
     req.body.image = req.files?.image;
-    console.log(req.files.image);
 
     const vacation = new VacationModel(req.body);
     const addedVacation = await vacationsLogic.createVacation(vacation);
