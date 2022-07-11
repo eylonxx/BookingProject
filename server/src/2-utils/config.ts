55bc5ccb9129c90da50f2a11c8f14bcd53b01e3d
Eylon <eylon.perets@gmail.com>
2022-07-11 18:23:12 +0300

    a

--- server/src/2-utils/config.ts
+++ server/src/2-utils/config.ts
@@ -19,13 +19,13 @@ class DevelopmentConfig extends Config {
 class ProductionConfig extends Config {
   public port = +process.env.PORT; // Will be set by the cloud
   public sqlHost = 'eu-cdbr-west-03.cleardb.net';
-  public sqlUser = 'bf4dde671cdab9';
-  public sqlPassword = '4dbc3b0d';
-  public sqlDatabase = 'heroku_fa919691a77cc5c';
+  public sqlUser = 'b99fdda40283ce';
+  public sqlPassword = 'd5969333';
+  public sqlDatabase = 'heroku_c751b26191864f0';
 }
 
 // Connection String
-//mysql://bf4dde671cdab9:4dbc3b0d@eu-cdbr-west-03.cleardb.net/heroku_fa919691a77cc5c?reconnect=true
+// mysql://b99fdda40283ce:d5969333@eu-cdbr-west-03.cleardb.net/heroku_c751b26191864f0?reconnect=true
 
 mysql: const config = process.env.NODE_ENV === 'development' ? new DevelopmentConfig() : new ProductionConfig();
 
